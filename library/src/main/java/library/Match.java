cc398f413fc2b0871af17149c581f3c18b53fa12
jandie <eidnaj@live.nl>
2017-05-03 10:54:41 +0200

    Match implemented

--- library/src/main/java/library/Match.java
+++ library/src/main/java/library/Match.java
@@ -4,4 +4,56 @@ package library;
  * Created by Jandie on 2017-05-03.
  */
 public class Match {
+    private int Id;
+    private String name;
+    private Table table;
+    private Team[] teams;
+
+    public Match(int id, String name, Table table, Team team1, Team team2) {
+        Id = id;
+        this.name = name;
+        this.table = table;
+
+        teams = new Team[2];
+        teams[0] = team1;
+        teams[1] = team2;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+    public Team[] getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Team[] teams) {
+        this.teams = teams;
+    }
+
+    public enum MatchType {
+        EIGHT_BALL,
+        NINE_BALL,
+        TEN_BALL
+    }
 }
