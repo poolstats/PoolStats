85e33d2086c77eee5b1def89db13aa94f59cd10a
guillaimejanssen <guillaimejanssen@msn.com>
2017-05-03 22:35:34 +0200

    Added ui module + started creating methods in the repo

--- repo/src/main/java/repo/UserRepo.java
+++ repo/src/main/java/repo/UserRepo.java
@@ -7,6 +7,7 @@ import library.UserStats;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,35 +15,17 @@ import java.util.List;
  * Created by Jandie on 2017-05-03.
  */
 public class UserRepo {
-    public static void main(String[] args) {
-        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("PoolStatsDB");
-        EntityManager entitymanager = emfactory.createEntityManager();
-        entitymanager.getTransaction().begin();
 
-        UserStats userStats1 = new UserStats(2, 3, 4, 5, 6, 7, 8, 9);
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
 
-        entitymanager.persist(userStats1);
+    public UserRepo() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("PoolStatsDB");
+        entityManager = entityManagerFactory.createEntityManager();
+    }
 
-        User user1 = new User("guillaime", userStats1);
-        User user2 = new User("michel", userStats1);
-
-        entitymanager.persist(user1);
-        entitymanager.persist(user2);
-
-        List<User> players = new ArrayList<>();
-        players.add(user1);
-        players.add(user2);
-
-        Team team = new Team("securoserve", players);
-
-        entitymanager.persist(team);
-
-        entitymanager.remove(entitymanager.find(Team.class, 304L));
-
-        System.out.println(entitymanager.find(UserStats.class, 1L).getBallsPotted());
-
-        entitymanager.getTransaction().commit();
-        entitymanager.close();
-        emfactory.close();
+    public void addUser(String username) {
+        entityManager.getTransaction().begin();
+        entityManager.getTransaction().commit();
     }
 }
---
+++ ui/src/main/java/ui/Application.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javafx.stage.Stage;
+
+/**
+ * Created by guillaimejanssen on 03/05/2017.
+ */
+public class Application extends javafx.application.Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
