063693e337d89c37d3f14c23a9e320b5b7e30b9f
jandie <eidnaj@live.nl>
2017-05-10 09:46:07 +0200

    Implemented UserLogic

--- logic/src/main/java/logic/UserLogic.java
+++ logic/src/main/java/logic/UserLogic.java
@@ -1,8 +1,33 @@
 package logic;
 
+import library.User;
+import logic.interfaces.IUser;
+import repo.UserRepo;
+
+import javax.persistence.NoResultException;
+
 /**
  * Created by Jandie on 2017-05-10.
  */
-public class UserLogic {
+public class UserLogic implements IUser {
+    private UserRepo userRepo;
 
+    public UserLogic() {
+        userRepo = new UserRepo();
+    }
+
+    @Override
+    public User getUser(String username) throws NoResultException {
+        return userRepo.getUser(username);
+    }
+
+    @Override
+    public void addUser(String username) {
+        userRepo.addUser(username);
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        userRepo.deleteUser(username);
+    }
 }
--- repo/src/main/java/repo/UserRepo.java
+++ repo/src/main/java/repo/UserRepo.java
@@ -24,17 +24,22 @@ public class UserRepo implements IUser {
     public User getUser(String username) throws NoResultException {
         Query query = connector.getEntityManager()
                 .createQuery("SELECT u FROM User u WHERE u.username = :username");
+
         query.setParameter("username", username);
+
         User returnValue = (User) query.getSingleResult();
+
         return returnValue;
     }
 
     @Override
     public void addUser(String username) {
         UserStats userStats = new UserStats(0, 0, 0, 0, 0, 0, 0, 0);
-        connector.getEntityManager().persist(userStats);
         User user = new User(username, userStats);
+
+        connector.getEntityManager().persist(userStats);
         connector.getEntityManager().persist(user);
+
         connector.commitTransaction();
     }
 
@@ -42,11 +47,13 @@ public class UserRepo implements IUser {
     public void deleteUser(String username) {
         Query query = connector.getEntityManager()
                 .createQuery("SELECT u FROM User u WHERE u.username = :username");
+
         query.setParameter("username", username);
         User users = (User) query.getSingleResult();
 
         connector.getEntityManager().remove(users);
         connector.getEntityManager().remove(users.getUserStats());
+
         connector.commitTransaction();
     }
 }
