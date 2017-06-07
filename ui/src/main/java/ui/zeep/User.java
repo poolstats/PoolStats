eccc06f69797be83bcf4b4a63e190467e21e8f6d
guillaimejanssen <guillaimejanssen@msn.com>
2017-06-07 10:06:03 +0200

    Soap service connected

---
+++ ui/src/main/java/ui/zeep/User.java
@@ -0,0 +1,54 @@
+
+package ui.zeep;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for user complex type.
+ * <p>
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;complexType name="user">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="username" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "user", propOrder = {
+        "username"
+})
+public abstract class User {
+
+    protected String username;
+
+    /**
+     * Gets the value of the username property.
+     *
+     * @return possible object is
+     * {@link String }
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the value of the username property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setUsername(String value) {
+        this.username = value;
+    }
+
+}
