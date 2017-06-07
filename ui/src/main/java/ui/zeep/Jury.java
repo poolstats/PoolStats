858de2e4e7391a91934f438b9812a34089ef9cbe
jandie <eidnaj@live.nl>
2017-06-07 11:47:44 +0200

    UI implementation of userService fixed

---
+++ ui/src/main/java/ui/zeep/Jury.java
@@ -0,0 +1,55 @@
+
+package ui.zeep;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for jury complex type.
+ * <p>
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;complexType name="jury">
+ *   &lt;complexContent>
+ *     &lt;extension base="{http://services/}user">
+ *       &lt;sequence>
+ *         &lt;element name="password" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "jury", propOrder = {
+        "password"
+})
+public class Jury
+        extends User {
+
+    protected String password;
+
+    /**
+     * Gets the value of the password property.
+     *
+     * @return possible object is
+     * {@link String }
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the value of the password property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setPassword(String value) {
+        this.password = value;
+    }
+
+}
